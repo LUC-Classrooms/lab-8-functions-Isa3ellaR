2d3346fa62387758ea30cc47d2ed49edd96500ac
Isabella Rodriguez <157334109+Isa3ellaR@users.noreply.github.com>
2024-03-20 23:53:16 +0000

    I added a bunny image for the myShape function

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ function draw() {
 
   //this is a call to myShape()
   myShape(width / 2, height / 2, 1);
-  myShape(width/3, height/2, .5)
-  myShape (width*2/3, height/2, 2)
+  myShape(width/3, height/2, .5);
+  myShape(width*2/3, height/2, .5);
+
+  //myShape(width/3, height/2, .5)
+  //myShape (width*2/3, height/2, 2)
   // myShape() takes three arguments
   // arg1: horizontal location
   // arg2: vertical location
@@ -20,11 +23,36 @@ function draw() {
 
 function myShape(x, y, s) {
   // make this function more interesting
-  let w = 50
+  let w = 100
   let h = 100
   push(); // make a separate layer
   translate(x, y); // move the origin point
   scale(s);
+
+  fill(245, 236, 211) //pale yellow
+  ellipse(-20, -60, 30, 100) //left ear
+  ellipse(20, -60, 30, 100) //right ear
+
+  fill(255, 176, 254) //pink
+  ellipse(-20,-60, 20, 80) //left inside of ear
+  ellipse(20, -60, 20, 80) //right inside of ear
+
+  fill(245, 237, 211) //pale yellow
   ellipse(0, 0, w, h); // simple ellipse at the translated origin (0,0)
+  fill(0) //black
+  ellipse(-20, -10, 30) //left eye
+  ellipse(20, -10, 30) //right eye
+
+  fill(247, 45, 78) //red
+  ellipse(-20, -10, 20) //left pupil
+  ellipse(20, -10, 20) //right pupil
+
+  fill(255, 176, 254) //pink
+  ellipse(0, 0, 10) //nose
+
+  fill(255) //white
+  rect(-10, 10, 10, 14) //bunny teeth
+  rect(0, 10, 10, 14)
+  
   pop(); // dispose of the layer
 }
